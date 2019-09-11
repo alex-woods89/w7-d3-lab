28b008c17ff26deb552f905a2b649352bf29ebde
Alex Woods <alex.woods89@hotmail.co.uk>
2019-09-11 15:07:05 +0100

    mvp started

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h1>Countries</h1>
+    <div class="main-container">
+      <countries-list :countries='countries'></countries-list>
+    </div>
+  </div>
+</template>
+
+<script>
+import CountriesList from './components/CountriesList.vue';
+import {eventBus} from './main.js';
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      countries: [],
+      selectedCountry: null
+    };
+  },
+  mounted(){
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(countries => this.countries = countries)
+
+    eventBus.$on('country-selected', (country) => {
+      this.selectedCountry = country
+    })
+  },
+  components: {
+    "countries-list": CountriesList
+  }
+}
+</script>
+
+<style>
+  .main-container {
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
